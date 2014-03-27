b3fd6ff7c5ef18c6142a44b3853f4e1251a747cb
Arne Meyer <meyer.arne83@gmail.com>
2014-03-27 20:30:18 +0100

    fix spelling in comment

--- numerik/polval.lisp
+++ numerik/polval.lisp
@@ -23,7 +23,7 @@
 ; THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (defun polval (x alist)
-  "compute the polynimial value of x by the coefficiants in alist. this can handle complex numbers"
+  "compute the polynomial value of x by the coefficiants in alist. this can handle complex numbers"
   (declare (number x))
   (loop for a number in (nreverse alist) 
      for i fixnum = 0 then (1+ i) 
