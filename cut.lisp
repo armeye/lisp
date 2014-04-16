6c5443fe48dc598ea6812d5b77bdc39ef3bbd728
Arne Meyer <meyer.arne83@gmail.com>
2014-04-16 20:58:53 +0200

    Add docstrings to cut.lisp

--- cut.lisp
+++ cut.lisp
@@ -1,10 +1,12 @@
 (defconstant +placeholder+ '<>)
 
 (defmacro cut (f &rest args)
+  "curry on steroids. you can curry arbitrary arguments of a function by substituting them with <>"
  (multiple-value-bind (pargs largs) (cut-helper args)
    `(lambda (,@largs) (funcall ,f ,@pargs))))
 
 (defun cut-helper (args)
+  "Helper function for cut. process a list args into args for the function and for the lambda. This will not be exported"
   (let ((args-processed (list))
 	(args-lambda (list)))
     (loop for i in args do (if (equalp i +placeholder+)
